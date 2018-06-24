a5ee02f46dece10bbc4ea93c12df284937ec5e84
Johan 't Hart <johanthart@gmail.com>
2018-06-24 23:11:48 +0200

    Draw links

--- src/app/rx-drawer/rx-drawer.component.ts
+++ src/app/rx-drawer/rx-drawer.component.ts
@@ -6,6 +6,12 @@ import { duration, ease } from '../tools';
 import { IRxNode, IRxConfig, IRxGenerator, IRxOperator, IRxOutput, IRxLink } from 'src/app/rxConfigDef';
 
 
+interface IMyLink {
+  xFrom: number;
+  yFrom: number;
+  xTo: number;
+  yTo: number;
+}
 
 @Component({
   selector: 'app-rx-drawer',
@@ -27,6 +33,7 @@ export class RxDrawerComponent implements OnInit {
   generators: IRxGenerator[];
   operators: IRxOperator[];
   output: IRxOutput[];
+  links: IMyLink[];
 
   constructor() {
   }
@@ -41,5 +48,13 @@ export class RxDrawerComponent implements OnInit {
     this.output = this.config.nodes
       .filter(v => v.type === 'output')
       .map(v => v as IRxOutput);
+    const ld = this.gmid + this.gmg;
+    this.links = this.config.links
+      .map(l => l.horizontal
+        ? { xFrom: l.pos.x * this.gpx - ld, yFrom: l.pos.y * this.gpx,
+            xTo: l.pos.x * this.gpx + ld, yTo: l.pos.y * this.gpx }
+        : { xFrom: l.pos.x * this.gpx, yFrom: l.pos.y * this.gpx - ld,
+            xTo: l.pos.x * this.gpx, yTo: l.pos.y * this.gpx + ld }
+      );
   }
 }

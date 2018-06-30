8b388c041ac2b3ca32f177cdfd1a0db2a637f311
Johan 't Hart <johanthart@gmail.com>
2018-06-30 23:45:03 +0200

    Drawer: grid calculations are now done in code

--- src/app/rx-drawer/rx-drawer.component.ts
+++ src/app/rx-drawer/rx-drawer.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { from } from 'rxjs';
-import { map, concatMap, repeat, share, distinctUntilChanged } from 'rxjs/operators';
 import EZ from 'eases';
 import { duration, ease } from '../tools';
-import { IRxNode, IRxConfig, IRxGenerator, IRxOperator, IRxOutput, IRxLink } from 'src/app/rxConfigDef';
+import { IRxConfig, IRxGenerator, IRxOperator, IRxOutput, IPos, IRxNodeBase } from 'src/app/rxConfigDef';
 
 
 interface IMyLink {
@@ -12,6 +10,81 @@ interface IMyLink {
   xTo: number;
   yTo: number;
 }
+
+interface IMyCircleNode {
+  center: IPos;
+  size: number;
+  name: string;
+}
+
+interface IMyRectNode {
+  tl: IPos;
+  textPos: IPos;
+  width: number;
+  height: number;
+  name: string;
+}
+
+interface IComponents {
+  // Objects
+  generators: IMyCircleNode[];
+  operators: IMyRectNode[];
+  output: IMyCircleNode[];
+  links: IMyLink[];
+}
+
+const makeInitial = (config: IRxConfig): IComponents => {
+  const gpx = 100; // grid pixel size
+  const gmg = 10;  // grid margin
+
+  const gmid = gpx / 2; // Middle of grid
+  const gmidToMar = gmid - gmg; // Distance from middle to margin
+  const ld = gmid + gmg;
+
+  const toDispCenter = (pos: IPos) => ({ x: pos.x * gpx, y: pos.y * gpx });
+  const toDispRect = (pos: IPos) => ({ x: pos.x * gpx - gmidToMar, y: pos.y * gpx - gmidToMar });
+  const toDispRectCenter = (pos: IPos) => ({ x: pos.x * gpx + gmid, y: pos.y * gpx });
+
+  const toCircleNode = (v: IRxNodeBase): IMyCircleNode => ({
+    center: toDispCenter(v.pos),
+    name: v.name,
+    size: gmidToMar
+  });
+
+  const toRectNode = (v: IRxNodeBase): IMyRectNode => ({
+    tl: toDispRect(v.pos),
+    textPos: toDispRectCenter(v.pos),
+    name: v.name,
+    width: gpx * 2 - gmg - gmg,
+    height: gpx - gmg - gmg
+  });
+
+  const generators = config.nodes
+    .filter(v => v.type === 'generator')
+    .map(toCircleNode);
+
+  const operators = config.nodes
+    .filter(v => v.type === 'operator')
+    .map(toRectNode);
+
+  const output = config.nodes
+    .filter(v => v.type === 'output')
+    .map(toCircleNode);
+
+  const links = config.links
+    .map(l => l.horizontal
+      ? { xFrom: l.pos.x * gpx - ld, yFrom: l.pos.y * gpx,
+          xTo: l.pos.x * gpx + ld, yTo: l.pos.y * gpx }
+      : { xFrom: l.pos.x * gpx, yFrom: l.pos.y * gpx - ld,
+          xTo: l.pos.x * gpx, yTo: l.pos.y * gpx + ld }
+    );
+  return {
+    generators,
+    operators,
+    output,
+    links
+  };
+};
 
 @Component({
   selector: 'app-rx-drawer',
@@ -22,39 +95,12 @@ export class RxDrawerComponent implements OnInit {
 
   @Input() config: IRxConfig;
 
-  // Metrics
-  gpx = 100; // grid pixel size
-  gmg = 10;  // grid margin
-
-  gmid = this.gpx / 2; // Middle of grid
-  gmidToMar = this.gmid - this.gmg; // Distance from middle to margin
-
-  // Objects
-  generators: IRxGenerator[];
-  operators: IRxOperator[];
-  output: IRxOutput[];
-  links: IMyLink[];
+  components: IComponents;
 
   constructor() {
   }
 
   ngOnInit() {
-    this.generators = this.config.nodes
-      .filter(v => v.type === 'generator')
-      .map(v => v as IRxGenerator);
-    this.operators = this.config.nodes
-      .filter(v => v.type === 'operator')
-      .map(v => v as IRxOperator);
-    this.output = this.config.nodes
-      .filter(v => v.type === 'output')
-      .map(v => v as IRxOutput);
-    const ld = this.gmid + this.gmg;
-    this.links = this.config.links
-      .map(l => l.horizontal
-        ? { xFrom: l.pos.x * this.gpx - ld, yFrom: l.pos.y * this.gpx,
-            xTo: l.pos.x * this.gpx + ld, yTo: l.pos.y * this.gpx }
-        : { xFrom: l.pos.x * this.gpx, yFrom: l.pos.y * this.gpx - ld,
-            xTo: l.pos.x * this.gpx, yTo: l.pos.y * this.gpx + ld }
-      );
+    this.components = makeInitial(this.config);
   }
 }
